90b1ac161d958cf562dd3b8fd177a18a7f18df1e
ninteo <hnt510@gmail.com>
2014-05-11 13:22:13 +0800

    finished

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+ * Copyright 2014 Huang Ning Tao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -242,8 +257,7 @@ namespace WindowsFormsApplication2
                 string clientcommand = System.Text.Encoding.UTF8.GetString(buffer).Substring(0, bufLen);
                 string[] stringSeparators = new string[] { "EOF" };
                 string[] split = clientcommand.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
-
-                if (split[4] == "IN\n")
+                if (split[4] == "INT")
                 {
                     //store username ipAddress pair so i can send info to corresponding client
                     hshTable.Add(split[0], clientep.Address);
@@ -255,7 +269,7 @@ namespace WindowsFormsApplication2
                         write_xml(split[0], split[1], split[2], split[3].Trim());
                     }));
                     keepalive = false;
-                }else if (split[4]=="OUT\n"){
+                }else if (split[4]=="OUT"){
                     Invoke(new MethodInvoker(delegate()
                     {
                         delete_xml(split[0], split[1], split[2], split[3]);
